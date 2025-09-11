182fe979c509d253ebf5d014d894f43452ba4f8a
Anna Harrison <16767360+annaharri89@users.noreply.github.com>
2025-09-10 23:29:31 -0400

    WIP: working on creating the mainactivity, mainscreen, and mainViewModel.

---
+++ app/src/main/java/com/example/stitchcounterv3/StitchCounterApp.kt
@@ -0,0 +1,8 @@
+package com.example.stitchcounterv3
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StitchCounterApp : Application()
+
---
+++ app/src/main/java/com/example/stitchcounterv3/feature/navigation/NavGraphs.kt
@@ -0,0 +1,7 @@
+package com.example.stitchcounterv3.feature.navigation
+
+import com.ramcosta.composedestinations.annotation.NavGraph
+
+@NavGraph
+annotation class RootNavGraph(val start: Boolean = false)
+
